b95107a50a3d4faf065992d9fd5c78eeb3e7a922
Michael Pavich <michael.pavich@porsche.digital>
2024-12-17 10:28:36 -0500

    added google analytics tracking with vite plugin, plan to track number of downloads and page visits

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { VitePluginRadar } from "vite-plugin-radar"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-9VHQ8VPQBX",
+      },
+    }),
+  ],
 })

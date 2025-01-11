9f5f22c7ae4ef61d5d1c7ea278f6a9b70c5454b0
Michael Pavich <michael.pavich@porsche.digital>
2025-01-10 20:22:41 -0500

    removed under construction component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { HomePage } from '@/pages/home'
-import { UnderConstructionPage } from "@/pages/underConstruction"
 import "./App.css"
 
 function App() {
-  const isProd = import.meta.env.PROD
 
   return (
     <>
-      {/* Temp show construction page for now */}
-      {isProd ? <UnderConstructionPage /> : <HomePage />}
+      <HomePage />
     </>
   )
 }
--- src/pages/underConstruction/underConstruction.tsx
+++
@@ -1,7 +0,0 @@
-import { UnderConstruction } from "@/components/underConstruction"
-
-export const UnderConstructionPage = () => {
-  return <>
-    <UnderConstruction />
-  </>
-}
